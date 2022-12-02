e3f64de60346444ee83e124aa15c11a3c54fe081
Marti Octtinger <martiocttinger@gmail.com>
2022-12-02 06:54:12 -0300

    Day 1

---
+++ day1/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>{{title}}</h1>
+  <ListaRegalos />
+</template>
+
+<script>
+import ListaRegalos from './components/ListaRegalos.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ListaRegalos
+  },
+  data(){
+    return{
+      title: 'Regalos'
+    }
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ day1/src/components/ListaRegalos.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul>
+    <li>Regalo 1</li>
+    <li>Regalo 2</li>
+    <li>Regalo 3</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'ListaRegalos',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
